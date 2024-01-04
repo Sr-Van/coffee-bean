1936d508b456dd2b99ce2aa6643c9784ec43f34d
Sr-Van <vanderson.dev1@gmail.com>
2024-01-04 11:25:02 -0300

    FIX: header menu links toggle

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -8,17 +8,17 @@ import { ActivatedRoute, Router, RouterModule } from '@angular/router';
     <nav class="navbar" #menu data-menu="closed">
       <h1 class="brand" routerLink="/">Coffee Bean</h1>
       <ul class="links">
-        <input class="checkbox" type="checkbox" (click)="toggleMenu(menu)" />
+        <input [checked]="accepted" class="checkbox" type="checkbox" (click)="toggleMenu(menu)" />
         <div class="hamburger-lines">
           <span class="line line1"></span>
           <span class="line line2"></span>
           <span class="line line3"></span>
         </div>
-        <li (click)="scrollTo('home')">Home</li>
-        <li (click)="scrollTo('shop')">Shop</li>
-        <li (click)="scrollTo('about')">About us</li>
+        <li (click)="scrollTo('home', menu)">Home</li>
+        <li (click)="scrollTo('shop', menu)">Shop</li>
+        <li (click)="scrollTo('about', menu)">About us</li>
         <li class="button">
-          <a class="order-btn" routerLink="order">Order Now</a>
+          <a class="order-btn" routerLink="order" (click)="goToOrder(menu)">Order Now</a>
         </li>
       </ul>
     </nav>
@@ -31,33 +31,40 @@ export class HeaderComponent {
   activedRoute = inject(ActivatedRoute)
   router = inject(Router)
 
-  ngOnInit() {
-    console.log(this.router.url);
+  accepted: boolean = false
 
+  goToOrder(menu: any) {
+    this.toggleMenu(menu)
+    this.accepted = false
   }
 
   toggleMenu(menu: any) {
 
     if (menu.dataset.menu === 'closed') {
-
+      this.accepted = true
       menu.dataset.menu = 'open'
       return
     }
-
+    this.accepted = false
     menu.dataset.menu = 'closed'
 
   }
 
-  scrollTo(el: any) {
+  scrollTo(el: any, menu: any) {
     if(this.router.url !== '/') {
-      console.log('diferente de barra');
+
       this.router.navigate(['/'])
       setTimeout(()=>{
         (document.getElementById(el) as HTMLElement).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+        this.toggleMenu(menu)
       }, 800)
       return
     }
     (document.getElementById(el) as HTMLElement).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+
+    setTimeout(() => {
+      this.toggleMenu(menu)
+    }, 200);
 
   }
 
